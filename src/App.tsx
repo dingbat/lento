cc2013d9457e6c17c15a8b87ef9b308b6c8a324a
dan hassin <danhassin@mac.com>
2021-03-05 11:08:47 -0500

    parse in ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, ChangeEvent } from 'react';
+import {useThrottleCallback} from '@react-hook/throttle';
+import styled from 'styled-components/macro';
+import parse from "./parse";
+
+const Main = styled.main`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+const Editor = styled.textarea`
+  flex: 1;
+  flex-shrink: 0;
+`;
+const Preview = styled.div`
+  flex: 1;
+`;
+
+const defaultText = `set bpm to 120
+beat/drums
+|xxxx|
+| x x|
+
+melody/synth
+
+main/
+play beat
+play drums| xxx|
+play melody 3 times
+loop reversed beat
+`;
 
 function App() {
+  const [code, setCode] = useState(defaultText);
+  const [ast, setAst] = useState<any>(null);
+  const generateAst = useThrottleCallback((code) => {
+    console.log(code);
+    setAst(parse(code));
+  });
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCode(e.target.value);
+    generateAst(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <Editor value={code} onChange={handleChange} />
+      <Preview>
+        <pre>{JSON.stringify(ast, null, 2)}</pre>
+      </Preview>
+    </Main>
   );
 }
 
